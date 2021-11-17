30d2a40539b665dc086cda18a2064f96a45049b0
Nate Starkey <gambini.nate@gmail.com>
2021-11-17 16:44:12 -0600

    Add self contained publish target

--- publish.bat
+++ publish.bat
@@ -3,4 +3,5 @@ set SCOREBINI_PUB_DIR="%~dp0publish"
 
 if exist %SCOREBINI_PUB_DIR% rmdir /q /s %SCOREBINI_PUB_DIR%
 
-dotnet publish Scorebini/Scorebini.csproj -c Release -p:PublishProfile=FolderProfile -o %SCOREBINI_PUB_DIR%
+dotnet publish Scorebini/Scorebini.csproj -c Release -p:PublishProfile=FolderProfile -o %SCOREBINI_PUB_DIR%\net5.0
+dotnet publish Scorebini/Scorebini.csproj -c Release -p:PublishProfile=FolderProfile --self-contained true -r win-x64 -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -o %SCOREBINI_PUB_DIR%\win-x64
